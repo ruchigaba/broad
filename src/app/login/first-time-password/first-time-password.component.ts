e3d5032f6bfde7e5976eb6f99955a8f9f78fe79f
ruchigaba <Radmin@2018>
2017-10-16 13:50:31 +0530

    firsttimelogin page

--- src/app/login/first-time-password/first-time-password.component.ts
+++ src/app/login/first-time-password/first-time-password.component.ts
@@ -1,15 +1,70 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { LoginComponent } from '../login.component';
+import { CommonAPICall } from '../../shared/shared.service';
 
 @Component({
   selector: 'app-first-time-password',
   templateUrl: './first-time-password.component.html',
-  styleUrls: ['./first-time-password.component.scss']
+  styleUrls: ['./first-time-password.component.scss'],
+  providers: [CommonAPICall]
 })
 export class FirstTimePasswordComponent implements OnInit {
+	type= "password";
+	show=false;
+    passResetMsg = sessionStorage.getItem('forgetPassFlag');
 
-  constructor() { }
+  constructor(private _route: Router, private _commonApiCall: CommonAPICall) { }
 
   ngOnInit() {
   }
+//-----------------------------------------CHANGE PASSWORD METHOD---------------------------------------
+  resetUserPassword(newPassword: string, reNewPassword: string) {
+      // if (oldPassword == undefined || oldPassword == '') {
+      //
+      //     this._commonApiCall.hideLoaderShowPopup("Enter Old Password", "Reset Password");
+      // }
+      if (newPassword == undefined || newPassword == '') {
+
+          this._commonApiCall.hideLoaderShowPopup("Enter New Password", "Reset Password");
+      }
+      else if(this._commonApiCall.checkPwd(newPassword) != ""){
+        this._commonApiCall.hideLoaderShowPopup(this._commonApiCall.checkPwd(newPassword),"Reset Password");
+      }
+      else if (reNewPassword == undefined || reNewPassword == '') {
+
+          this._commonApiCall.hideLoaderShowPopup("Enter New Password", "Reset Password");
+      }
+      else {
+          if (newPassword.trim() !== reNewPassword.trim()) {
+
+              this._commonApiCall.hideLoaderShowPopup("Passwords must be same.", "Reset Password");
+          }
+          else {
+              var token = sessionStorage.getItem('token');
+              var data =
+                  {
+                      "userName": sessionStorage.getItem('userName'),
+                      "oldPassword": sessionStorage.getItem('local_tempPassword'),
+                      "newPassword": newPassword
+                  }
+              this._commonApiCall.putService("users/passwords/firsttime", "Bearer " + token, "application/json", data)
+                  .subscribe(res => {
+
+                    this._commonApiCall.hideLoaderShowPopup(res.result, "Reset Password");
+                  //  this._route.navigate(['']);
+                   // var loginObject = new LoginComponent(this._route,this._commonApiCall);
+                   // loginObject.loginService(sessionStorage.getItem('userName'),newPassword);
+
+                  },
+                error =>{
+                  this._commonApiCall.handleError(error, "Reset Password");
+                })
+          }
+      }
+}
+goToLogin() {
+  this._route.navigate(['']);
+}
 
 }
