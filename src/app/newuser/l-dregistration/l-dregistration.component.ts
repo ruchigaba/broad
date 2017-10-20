00c7c5d84f4cf8d19573cd6b63621b7d174762f7
khushboo-Yadav123 <khushboo.yadav@primussoft.com>
2017-10-20 07:37:26 -0400

    pull

--- src/app/newuser/l-dregistration/l-dregistration.component.ts
+++ src/app/newuser/l-dregistration/l-dregistration.component.ts
@@ -14,10 +14,14 @@ questions: any = [];
  checkedQues = false;
  inputElem: any= [];
   index: any = [];
-   count: number = 0;
+  count: number = 0;
+  firstName: string;
+  lastName: string;
+  email: string;
+  reEmail:string;
    claimnum:string;
   constructor(private _commonApiCall: CommonAPICall,private router: Router) { 
-    // this.question();
+     this.question();
      this.commnFunc = new CommonFunction();
    
   }
@@ -25,8 +29,33 @@ questions: any = [];
      this.claimnum= sessionStorage.getItem("claimno");
   }
   createNewUser12(){
-    
-      this.router.navigate(['./securityquestions']);
+     var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    if (this.firstName == "" || this.firstName == undefined) {
+      this.commnFunc.alertPopup("Please fill the First Name","Registration");
+    }
+    else if (this.lastName == "" || this.lastName == undefined) {
+      this.commnFunc.alertPopup("Please fill the Last Name","Registration");
+    }
+    else if (this.email == "" || this.email == undefined || !re.test(this.email)) {
+      this.commnFunc.alertPopup("Please fill the email in valid format","Registration");
+    }
+    else if (this.reEmail == "" || this.reEmail == undefined || !re.test(this.reEmail)) {
+      this.commnFunc.alertPopup("Please fill the Re-Enter Email in valid format","Registration");
+    }
+    else if( this.email != this.reEmail) { 
+   this.commnFunc.alertPopup("Your Email and Re-enter email does not match.", "Registration" );
+    }
+    else if (this.inputElem[this.index[0]] == "" || this.inputElem[this.index[0]] == undefined) {
+      this.commnFunc.alertPopup("Security answers are required","Registration");
+    }
+    else if (this.inputElem[this.index[1]] == "" || this.inputElem[this.index[1]] == undefined) {
+      this.commnFunc.alertPopup("Security answers are required","Registration");
+    }
+    else if (this.inputElem[this.index[2]] == "" || this.inputElem[this.index[2]] == undefined) {
+      this.commnFunc.alertPopup("Security answers are required","Registration");
+    }
+    //  this.router.navigate(['./securityquestions']);
+    else{}
   }
 gotoHelp() {
     this.router.navigate(['./help']);
@@ -34,42 +63,42 @@ gotoHelp() {
   goToLogin() {
     this.router.navigate(['']);
   }
-  //  question(): any {
-  //   this._commonApiCall.getService("SecQuestions", "", "")
-  //     .subscribe(res => {
-  //       this.questions = res.result.secquestions;
-  //     },
-  //   error => {
-  //     this._commonApiCall.handleError(error,"Security questions");
-  //   });
-  // }
-  // customTrackBy(index: number, obj: any): any {
-  //   return index;
-  // }
-  //  checkValue(e: any, i: any) {
-  //   this.checkedQues = e.target.checked;
+   question(): any {
+    this._commonApiCall.getService("SecQuestions", "", "")
+      .subscribe(res => {
+        this.questions = res.result.secquestions;
+      },
+    error => {
+      this._commonApiCall.handleError(error,"Security questions");
+    });
+  }
+  customTrackBy(index: number, obj: any): any {
+    return index;
+  }
+   checkValue(e: any, i: any) {
+    this.checkedQues = e.target.checked;
 
-  //   if (this.checkedQues) {
-  //     this.index.push(i);
-  //     console.log("push  "+i);
-  //     console.log("indexlength  " + this.index.length);
-  //     this.count++;
-  //     document.getElementById("securityAns" + i).style.display = '';
-  //     if (this.count > 3) {
-  //       this.count--;
-  //         this.index.splice(this.index.indexOf(i),1);
-  //       console.log("indexlength  " + this.index.length);
-  //       e.target.checked = false;
-  //       document.getElementById("securityAns" + i).style.display = 'none';
-  //       this.commnFunc.alertPopup("Select not more than 3 Questions","Registration");
-  //     }
-  //   }
-  //   else {
-  //     this.index.splice(this.index.indexOf(i), 1);
-  //     console.log("pop  "+i);
-  //     console.log("indexlength  " + this.index.length);
-  //     this.count--;
-  //     document.getElementById("securityAns" + i).style.display = 'none';
-  //   }
-  // }
+    if (this.checkedQues) {
+      this.index.push(i);
+      console.log("push  "+i);
+      console.log("indexlength  " + this.index.length);
+      this.count++;
+      document.getElementById("securityAns" + i).style.display = '';
+      if (this.count > 3) {
+        this.count--;
+          this.index.splice(this.index.indexOf(i),1);
+        console.log("indexlength  " + this.index.length);
+        e.target.checked = false;
+        document.getElementById("securityAns" + i).style.display = 'none';
+        this.commnFunc.alertPopup("Select not more than 3 Questions","Registration");
+      }
+    }
+    else {
+      this.index.splice(this.index.indexOf(i), 1);
+      console.log("pop  "+i);
+      console.log("indexlength  " + this.index.length);
+      this.count--;
+      document.getElementById("securityAns" + i).style.display = 'none';
+    }
+  }
 }
