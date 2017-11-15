53af3ea3cd0d8e7ff0ce87abe0415d26a088a13f
khushboo-Yadav123 <khushboo.yadav@primussoft.com>
2017-11-15 07:11:08 -0500

    pull

--- src/app/dashboard/settings/settings.component.ts
+++ src/app/dashboard/settings/settings.component.ts
@@ -14,27 +14,6 @@ title = '';
   constructor(private _routes : Router,private route: ActivatedRoute) { }
 
   ngOnInit() {
-   // document.getElementById('main-container').style.marginLeft='259px';
-  //  if(document.getElementById("worker").className=="list-group-item-mycom sjsj"){
-  //     var x= document.getElementById("worker")
-  //           console.log(x);
-  // //     //  x.style.border="0px";
-  if(sessionStorage.getItem('Id')=="workerscomp@gmail.com"){
-  document.getElementById("sidebar").style.display = 'none';
-  
-   document.getElementById("mobileFooter").style.display = 'none';
-
-}
-else if(sessionStorage.getItem('Id')=="onlyleavedisability@gmail.com"){
-  document.getElementById("sidebar").style.display = 'none';
-   document.getElementById("mobileFooter").style.display = 'none';
-}
-else if(sessionStorage.getItem('userName')!="" && sessionStorage.getItem('Id')!="onlyworkerscomp@gmail.com"){
-   document.getElementById("sidebar").style.display = 'none';
-    document.getElementById("mobileFooter").style.display = 'none';
-}
-
-
      let currentRoute = this.route.root;
       while (currentRoute.children[0] !== undefined) {
             currentRoute = currentRoute.children[0];
@@ -43,6 +22,28 @@ else if(sessionStorage.getItem('userName')!="" && sessionStorage.getItem('Id')!=
         sessionStorage.setItem("parent",this.data.parent);
                     //add title to header
           this.title = this.data.title;
+   // document.getElementById('main-container').style.marginLeft='259px';
+  //  if(document.getElementById("worker").className=="list-group-item-mycom sjsj"){
+  //     var x= document.getElementById("worker")
+  //           console.log(x);
+  // //     //  x.style.border="0px";
+  if(sessionStorage.getItem('Id')=="workerscomp@gmail.com"){
+  document.getElementById("sidebar").style.display = 'none';
+  
+  // document.getElementById("mobileFooter").style.display = 'none';
+
+}
+else if(sessionStorage.getItem('Id')=="onlyleavedisability@gmail.com"){
+  document.getElementById("sidebar").style.display = 'none';
+ //  document.getElementById("mobileFooter").style.display = 'none';
+}
+else if(sessionStorage.getItem('userName')!="" && sessionStorage.getItem('Id')!="onlyworkerscomp@gmail.com"){
+   document.getElementById("sidebar").style.display = 'none';
+  //  document.getElementById("mobileFooter").style.display = 'none';
+}
+
+
+    
   //  }
     document.body.className = 'settings';
     window.scrollTo(0, 0);
